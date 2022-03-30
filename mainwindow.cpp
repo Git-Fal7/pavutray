3fc77daf3aaffcfbb1c5ef9e4fd1412940ffb703
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-03-30 15:05:14 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include <QDebug>
 #include <QMessageBox>
 #include <QCoreApplication>
+#include <QProcess>
 
 #include "mainwindow.h"
 
@@ -8,11 +9,8 @@ MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
   , trayIcon(new QSystemTrayIcon(this))
 {
-  // Tray icon menu
-  auto menu = this->createMenu();
-  this->trayIcon->setContextMenu(menu);
 
-  // App icon
+    // App icon
   auto appIcon = QIcon(":/icons/heart.png");
   this->trayIcon->setIcon(appIcon);
   this->setWindowIcon(appIcon);
@@ -24,23 +22,11 @@ MainWindow::MainWindow(QWidget *parent)
   connect(trayIcon, &QSystemTrayIcon::activated, this, &MainWindow::iconActivated);
 }
 
-QMenu* MainWindow::createMenu()
-{
-  // App can exit via Quit menu
-  auto quitAction = new QAction("&Quit", this);
-  connect(quitAction, &QAction::triggered, qApp, &QCoreApplication::quit);
-
-  auto menu = new QMenu(this);
-  menu->addAction(quitAction);
-
-  return menu;
-}
-
 void MainWindow::iconActivated(QSystemTrayIcon::ActivationReason reason_)
 {
   switch (reason_) {
   case QSystemTrayIcon::Trigger:
-    this->trayIcon->showMessage("Hello", "You clicked me!");
+    QProcess::startDetached("pavucontrol-qt");
     break;
   default:
     ;
